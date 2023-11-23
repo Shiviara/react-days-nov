9b3c033160116add870ab9c64f848ccf444b6e81
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-11-23 10:26:54 +0100

    text

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>I'm trying to not give up</h2>
+      <h2>I'm trying to not give up!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
