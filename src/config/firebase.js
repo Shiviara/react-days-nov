e90cd7b3bc5f9b03286df8720ef9ca00c600bae9
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-11-23 09:46:20 +0100

    more firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSeFrk6cmqLCNor3LeN84ShInkbMk5QJI",
+  authDomain: "react-november.firebaseapp.com",
+  projectId: "react-november",
+  storageBucket: "react-november.appspot.com",
+  messagingSenderId: "308233074580",
+  appId: "1:308233074580:web:ba8ba6046604ae5e268181",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
